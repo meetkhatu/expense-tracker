23b7ee94230b2ada875286f3071b71709db69a9f
meetkhatu <143870423+meetkhatu@users.noreply.github.com>
2025-06-09 15:01:29 +0530

    sidemenu changes

--- frontend/expense-tracker/src/components/layouts/Sidemenu.jsx
+++ frontend/expense-tracker/src/components/layouts/Sidemenu.jsx
@@ -25,7 +25,7 @@ const Sidemenu = ({ activeMenu }) => {
     navigate('/login')
   }
   return (
-    <div className='w-64 h-[calc(100vh-61vh)] bg-white border-r border-gray-200/50 p-5 sticky top-[61px] z-20'>
+    <div className='w-64 h-auto bg-white border-r border-gray-200/50 p-5 sticky top-[61px] z-20'>
       <div className='flex flex-col items-center justify-center gap-3 mt-3 mb-7'>
         {
           user?.profileImageUrl ? (
