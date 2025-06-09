76de71e07d02c3c098f9a82c186101ed1ba5f3c3
meetkhatu <143870423+meetkhatu@users.noreply.github.com>
2025-06-09 13:51:07 +0530

    changes in api_paths for deployment

--- frontend/expense-tracker/src/utils/apiPaths.js
+++ frontend/expense-tracker/src/utils/apiPaths.js
@@ -1,27 +1,27 @@
-export const BASE_URL = "http://localhost:8000"
+export const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8000";
 
 export const API_PATHS = {
-    AUTH: {
-        LOGIN: "/api/v1/auth/login",
-        REGISTER: "/api/v1/auth/register",
-        GET_USER_INFO: "/api/v1/auth/getUser"
-    },
-    DASHBOARD: {
-        GET_DATA: "/api/v1/dashboard/"
-    },
-    INCOME: {
-        ADD_INCOME: "/api/v1/income/add",
-        DELETE_INCOME: (incomeId) => `/api/v1/income/${incomeId}`,
-        GET_ALL_INCOME: "/api/v1/income/get",
-        DOWNLOAD_INCOME: "/api/v1/income/downloadexcel"
-    },
-    EXPENSE: {
-        ADD_EXPENSE: "/api/v1/expense/add",
-        DELETE_EXPENSE: (expenseId) => `/api/v1/expense/${expenseId}`,
-        GET_ALL_EXPENSE: "/api/v1/expense/get",
-        DOWNLOAD_EXPENSE: "/api/v1/expense/downloadexcel"
-    },
-    IMAGE: {
-        UPLOAD_IMAGE: "/api/v1/auth/upload-image"
-    }
-}
+  AUTH: {
+    LOGIN: `${BASE_URL}/api/v1/auth/login`,
+    REGISTER: `${BASE_URL}/api/v1/auth/register`,
+    GET_USER_INFO: `${BASE_URL}/api/v1/auth/getUser`
+  },
+  DASHBOARD: {
+    GET_DATA: `${BASE_URL}/api/v1/dashboard/`
+  },
+  INCOME: {
+    ADD_INCOME: `${BASE_URL}/api/v1/income/add`,
+    DELETE_INCOME: (incomeId) => `${BASE_URL}/api/v1/income/${incomeId}`,
+    GET_ALL_INCOME: `${BASE_URL}/api/v1/income/get`,
+    DOWNLOAD_INCOME: `${BASE_URL}/api/v1/income/downloadexcel`
+  },
+  EXPENSE: {
+    ADD_EXPENSE: `${BASE_URL}/api/v1/expense/add`,
+    DELETE_EXPENSE: (expenseId) => `${BASE_URL}/api/v1/expense/${expenseId}`,
+    GET_ALL_EXPENSE: `${BASE_URL}/api/v1/expense/get`,
+    DOWNLOAD_EXPENSE: `${BASE_URL}/api/v1/expense/downloadexcel`
+  },
+  IMAGE: {
+    UPLOAD_IMAGE: `${BASE_URL}/api/v1/auth/upload-image`
+  }
+};
